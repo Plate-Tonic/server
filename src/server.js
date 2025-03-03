5fd54ccfbcf953c1cffed5c9db5986609c60b3cd
chung-v <vcdott@gmail.com>
2025-03-03 15:27:57 +1100

    feat: configured app routes

--- src/server.js
+++ src/server.js
@@ -3,9 +3,30 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const app = express();
-
 app.use(express.json());
-app.use(cors());
+
 app.use(helmet());
+
+let corsOptions = {
+    origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://reactapp.com"],
+    methods: ["GET", "POST"]
+};
+app.use(cors(corsOptions));
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "Test!"
+    });
+});
+
+const authRoutes = require("./routes/AuthRoutes.js");
+app.use("/", authRoutes);
+
+const userRoutes = require("./routes/UserRoutes.js");
+app.use("/user", userRoutes);
+
+const blogPostRoutes = require("./routes/BlogPostRoutes.js");
+app.use("/blog", blogPostRoutes);
+
 
 module.exports = { app };
