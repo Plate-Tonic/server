56f604bc8207f3e9670404654a06b7e7ae6802e4
chung-v <vcdott@gmail.com>
2025-03-04 13:45:37 +1100

    feat: added asyncHandler for auth routes and controllers

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
+
 const { registerUser, loginUser } = require("../controllers/AuthController");
 
 const router = express.Router();
 
 // Register a new user
-router.post("/register", registerUser);
+router.post("/register", asyncHandler(registerUser));
 
 // Login an existing user
-router.post("/login", loginUser);
+router.post("/login", asyncHandler(loginUser));
 
 module.exports = router;
