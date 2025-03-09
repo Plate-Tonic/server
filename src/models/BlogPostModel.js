0c48f0c40786c53b3e3cba1b91738ae39481efac
chung-v <vcdott@gmail.com>
2025-03-09 19:46:46 +1100

    refactor: cleaned up code for model files

--- src/models/BlogPostModel.js
+++ src/models/BlogPostModel.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 
+// Schema for blog posts
 const BlogPostSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        unique: true,
-        minLength: 10
+        unique: true
     },
     author: {
         type: String,
         required: true
     },
-    date: {
-        type: Date,
-        default: Date.now
-    },
     content: {
-        type: String
+        type: String,
+        required: true
     },
     tags: {
         type: [String],
+        required: true,
         default: []
+
     }
 });
 
+// Model for blog posts
 const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
 
 module.exports = {
