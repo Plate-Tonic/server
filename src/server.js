a9423ea142f9bc14eba2308652a3cd486c5cf8e2
montsieur <dndott@gmail.com>
2025-03-09 22:50:17 +1100

    Feature: Changed Upload Image for Meals to file upload and links with DB

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path"); // Move this line here
 
 const app = express();
 app.use(express.json());
 
 app.use(helmet());
 
+// General CORS configuration for all other routes
 let corsOptions = {
     origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://platetonic.netlify.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]  // Allow specific headers
+
 };
 app.use(cors(corsOptions));
+
+// Serve static files from /uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.get("/", (request, response) => {
     response.json({
--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Function to connect to the database
 async function dbConnect() {
-    let databaseUrl = process.env.DATABASE_URL; // || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
+    let databaseUrl = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
 
     try {
         console.log("Connecting to database:", databaseUrl);
